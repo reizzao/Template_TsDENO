1403787c28c8775d66066dc47d1ef5549bb80d9a
reizzao <reizao1977@gmail.com>
2025-05-14 13:25:50 -0300

    feat: estrutura de modelo da entidade Example1

---
+++ Project/src/Domain/Model/Example1.ts
@@ -0,0 +1,17 @@
+
+interface Example1PropsModel {
+  ID: string
+  Nome: string
+  Sobrenome: string
+}
+
+class Example1 {
+  // public readonly Props: Example1PropsModel
+
+  constructor(private readonly Props: Example1PropsModel,) {
+    this.Props.ID = Props.ID || "todo: makeID() - chamar aqui"
+  }
+
+}
+
+export { Example1 }
